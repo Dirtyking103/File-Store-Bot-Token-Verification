f03761bd4f8f863c4de874c19e32b84ab5de34a9
Dirtyking103 <dirtyking103@gmail.com>
2024-09-01 16:02:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,8 +13,8 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "7202892465"))
 PORT = os.environ.get("PORT", "8080")
 
 #Database 
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://dirtyking102:KLT5HXFTfou4QieE@dirtyking.gdvsj.mongodb.net/?retryWrites=true&w=majority&appName=Dirtyking")
-DB_NAME = os.environ.get("DATABASE_NAME", "Dirtyking")
+DB_URI = os.environ.get("DATABASE_URL", "")
+DB_NAME = os.environ.get("DATABASE_NAME", "")
 
 #Shortner (token system) 
 
